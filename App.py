82368b27fd6e5d453efad240e4bdeacd14a820e4
Mostafa Mahmoud <61292945+MostafaAbbas-git@users.noreply.github.com>
2021-04-24 21:44:27 +0200

    Autologging
    added "extras" folder

--- App.py
+++ App.py
@@ -1,22 +1,24 @@
-import math
-import os
 import sys
 from PyQt5 import QtCore, QtWidgets, QtGui, uic
 import logging
+from autologging import logged, TRACE, traced
 
 # Create and configure logger
-LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+LOG_FORMAT = "%(levelname)s:%(filename)s,%(lineno)d:%(name)s.%(funcName)s:%(message)s"
 logging.basicConfig(filename="FTMIXER.log",
-                    level=logging.DEBUG,
+                    level=TRACE,
                     format=LOG_FORMAT,
                     filemode='w')
 logger = logging.getLogger()
 
 
+@ traced
+@ logged
 class FTMixer(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = uic.loadUi('FTMixer.ui', self)
+        self.__log.info("initialized")
 
 
 def main():
