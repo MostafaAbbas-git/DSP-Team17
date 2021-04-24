b587c4d4efa304f530c5724185f352de80695269
sheren390 <61457923+sheren390@users.noreply.github.com>
2021-04-25 01:03:53 +0200

    (loading images) done

--- App.py
+++ App.py
@@ -1,10 +1,20 @@
-import sys
-from PyQt5 import QtCore, QtWidgets, QtGui, uic
-import logging
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
+from PyQt5.QtWidgets import QFileDialog, QGraphicsScene, QMessageBox
+from pyqtgraph import PlotWidget, PlotItem
+from PyQt5.QtCore import QSettings
 from autologging import logged, TRACE, traced
-import numpy as np
 from numpy.fft import fft2, ifft2, fftfreq
 from numpy.lib.type_check import imag
+import numpy as np
+import math
+import cv2 as cv
+import sys
+import pandas as pd
+import pyqtgraph as pg
+import os
+import logging
+
+
 # Create and configure logger
 LOG_FORMAT = "%(levelname)s:%(filename)s,%(lineno)d:%(name)s.%(funcName)s:%(message)s"
 logging.basicConfig(filename="FTMixer.log",
@@ -16,12 +26,61 @@ logger = logging.getLogger()
 
 @ traced
 @ logged
-class FTMixer(QtWidgets.QMainWindow):
+class ImagesMixer(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = uic.loadUi('FTMixer.ui', self)
         self.__log.info("initialized")
 
+        
+        # connecting buttons
+        self.loadImgs = [self.Open_Img1, self.Open_Img2]
+
+        self.loadImgs[0].triggered.connect(lambda: self.Open(0))
+        self.loadImgs[1].triggered.connect(lambda: self.Open(1))
+        self.loadImgs[1].setDisabled(True)
+
+        self.displays = [self.fixedDisplay_1, self.fixedDisplay_2, self.selectedDisplay_1,
+                         self.selectedDisplay_2, self.output1_Display, self.output2_Display]
+
+        for i in range(len(self.displays)):
+            self.displays[i].ui.histogram.hide()
+            self.displays[i].ui.roiBtn.hide()
+            self.displays[i].ui.menuBtn.hide()
+            self.displays[i].ui.roiPlot.hide()
+
+    def Open(self, index):
+        selected_image = QtGui.QFileDialog.getOpenFileName(
+            self, 'Select image', os.getenv('HOME'), "Images (*.png *.xpm *.jpg)")
+
+        self.path = selected_image[0]
+        self.imgByte = cv.cvtColor(cv.imread(self.path), cv.COLOR_BGR2GRAY)
+        # shape method return tuple of number of rows, columns, and channels (if the image is color)
+        self.shape = self.imgByte.shape
+        #self.image = self.imageModel(self.path)
+
+        if index == 0:
+            self.Img1_Size = self.shape
+            self.loading(index)
+            self.loadImgs[1].setDisabled(False)
+
+        elif index != 0:
+            if self.shape != self.Img1_Size:
+                #print('Error the images are not the same size')
+                self.msg.setWindowTitle("Error in Image Size")
+                self.msg.setText("The 2 images must have the same size")
+                self.msg.setIcon(QMessageBox.Warning)
+                x = self.msg.exec_()
+                return
+            else:
+                self.loading(index)
+
+    def loading(self, index):
+        self.displays[index].show()
+        self.displays[index].setImage(self.imgByte.T)
+        
+        
+        
 
     def get_fft(self, data_array):
         #Fourier transform of given data array
@@ -45,12 +104,14 @@ class FTMixer(QtWidgets.QMainWindow):
     def get_ifft (self, data_array):
         return ifft2(data_array)
 
+      
+   
 def main():
     app = QtWidgets.QApplication(sys.argv)
     app.setOrganizationName("CUFE")
     app.setOrganizationDomain("CUFEDomain")
     app.setApplicationName("Fourier Transform Mixer")
-    application = FTMixer()
+    application = ImagesMixer()
     application.show()
     app.exec_()
 
