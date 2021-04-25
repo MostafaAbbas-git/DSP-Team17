2da51b99c0371ba6d3212ede0bb53a67cd17e641
Mostafa Mahmoud <61292945+MostafaAbbas-git@users.noreply.github.com>
2021-04-25 10:39:50 +0200

    Size warning msg

--- App.py
+++ App.py
@@ -4,20 +4,16 @@ from pyqtgraph import PlotWidget, PlotItem
 from PyQt5.QtCore import QSettings
 from autologging import logged, TRACE, traced
 from numpy.fft import fft2, ifft2, fftfreq
-from numpy.lib.type_check import imag
 import numpy as np
-import math
 import cv2 as cv
 import sys
-import pandas as pd
-import pyqtgraph as pg
 import os
 import logging
 
 
 # Create and configure logger
 LOG_FORMAT = "%(levelname)s:%(filename)s,%(lineno)d:%(name)s.%(funcName)s:%(message)s"
-logging.basicConfig(filename="FTMixer.log",
+logging.basicConfig(filename="ImagesMixer.log",
                     level=TRACE,
                     format=LOG_FORMAT,
                     filemode='w')
@@ -31,26 +27,51 @@ class ImagesMixer(QtWidgets.QMainWindow):
         super().__init__()
         self.ui = uic.loadUi('FTMixer.ui', self)
         self.__log.info("initialized")
+        self.settings = QSettings("FT Images Mixer", 'App')
 
-        
-        # connecting buttons
-        self.loadImgs = [self.Open_Img1, self.Open_Img2]
+        try:
+            # Saving the last position and size of the application window
+            self.resize(self.settings.value('window size'))
+            self.move(self.settings.value('window position'))
+        except:
+            pass
+
+        # Creating warning msg for image-2 size
+        self.size_warning_msg = QMessageBox()
+        self.size_warning_msg.setWindowTitle("Error in Image Size")
+        self.size_warning_msg.setText("The 2 images must have the same size")
+        self.size_warning_msg.setIcon(QMessageBox.Warning)
+
+        self.loadImgs = [self.actionOpenImg1, self.actionOpenImg2]
+
+        self.displays = [self.fixedDisplay_1, self.fixedDisplay_2, self.selectedDisplay_1,
+                         self.selectedDisplay_2, self.output1_Display, self.output2_Display]
+
+        ## Connecting Buttons ##
+
+        self.actionClear.triggered.connect(lambda: self.clearall())
+        self.actionNewWindow.triggered.connect(lambda: self.make_new_window())
 
         self.loadImgs[0].triggered.connect(lambda: self.Open(0))
         self.loadImgs[1].triggered.connect(lambda: self.Open(1))
         self.loadImgs[1].setDisabled(True)
 
-        self.displays = [self.fixedDisplay_1, self.fixedDisplay_2, self.selectedDisplay_1,
-                         self.selectedDisplay_2, self.output1_Display, self.output2_Display]
-        
-        self.actionClear.triggered.connect(lambda: self.clearall())
-
-
+        # Adjusting QtPlotWidgets to only show images
         for i in range(len(self.displays)):
             self.displays[i].ui.histogram.hide()
             self.displays[i].ui.roiBtn.hide()
             self.displays[i].ui.menuBtn.hide()
             self.displays[i].ui.roiPlot.hide()
+
+    # Related to QSettings
+    def closeEvent(self, event):
+        self.settings.setValue('window size', self.size())
+        self.settings.setValue('window position', self.pos())
+
+    # Creating new window by calling the main class
+    def make_new_window(self):
+        self.new_win = ImagesMixer()
+        self.new_win.show()
 
     def Open(self, index):
         selected_image = QtGui.QFileDialog.getOpenFileName(
@@ -69,11 +90,8 @@ class ImagesMixer(QtWidgets.QMainWindow):
 
         elif index != 0:
             if self.shape != self.Img1_Size:
-                #print('Error the images are not the same size')
-                self.msg.setWindowTitle("Error in Image Size")
-                self.msg.setText("The 2 images must have the same size")
-                self.msg.setIcon(QMessageBox.Warning)
-                x = self.msg.exec_()
+                # Showing size warning msg
+                self.size_warning_msg.exec_()
                 return
             else:
                 self.loading(index)
@@ -81,36 +99,35 @@ class ImagesMixer(QtWidgets.QMainWindow):
     def loading(self, index):
         self.displays[index].show()
         self.displays[index].setImage(self.imgByte.T)
-        
-        
-        
 
     def get_fft(self, data_array):
-        #Fourier transform of given data array
+        # Fourier transform of given data array
         self.fft_data = fft2(data_array)
-        #separate the magnitude
+        # separate the magnitude
         self.fft_data_mag = np.abs(self.fft_data)
-        #separate the phase
+        # separate the phase
         self.fft_data_phase = np.angle(self.fft_data)
-        #separate the real components
+        # separate the real components
         self.fft_data_real = np.real(self.fft_data)
-        #separate the imaginary components
+        # separate the imaginary components
         self.fft_data_imag = np.imag(self.fft_data)
         # The Discrete Fourier Transform sample frequencies
         sample_freq = fftfreq(self.fft_data.size)
-        #list of lists holds all calculated values
+        # list of lists holds all calculated values
         FT_list = [self.fft_data_mag, self.fft_data_phase,
-                self.fft_data_real, self.fft_data_imag, sample_freq]
-        #return the list of lists
+                   self.fft_data_real, self.fft_data_imag, sample_freq]
+        # return the list of lists
         return FT_list
 
-    def get_ifft (self, data_array):
+    def get_ifft(self, data_array):
         return ifft2(data_array)
 
+#Fixed display shouldn't be cleared #################
     def clearall(self):
         for i in range(6):
-            self.displays[i].clear()      
-   
+            self.displays[i].clear()
+
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
     app.setOrganizationName("CUFE")
