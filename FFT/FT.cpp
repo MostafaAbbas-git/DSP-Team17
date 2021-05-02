db8760efb8e875f59c872572bbf31e01cc36ef96
Aya Ehab <61461463+ayaehab@users.noreply.github.com>
2021-05-02 22:28:07 +0200

    Part B

---
+++ FFT/FT.cpp
@@ -0,0 +1,82 @@
+#include <math.h>
+#include <complex>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+
+#define M_PI 3.14159265358979323846
+
+using namespace std;
+
+extern "C"
+{
+    void FFT(vector<complex<double>> & fftArray );
+    vector<complex<double>> DFT(vector<complex<double>> data);
+}
+
+void FFT (vector<complex<double>> & fftArray){
+    
+    int N = fftArray.size();
+
+    if( N == 1) return;
+
+    vector<complex<double>> even,odd;
+
+    // Splitting
+    for(int i = 0; i != N/2; i++){
+
+        even.push_back( fftArray[2*i] );
+        odd.push_back(fftArray [2*i +1]);
+    } 
+
+    /* perform the recursive FFT operations on the splitted even and odd vectors 
+    *  to split each of them to another even and odd vectors,
+    *  until we have one element only that can't be splitted anymore. 
+    */
+    FFT(even);
+    FFT(odd);
+
+    
+    // Compining the small vectors to make the final fft array
+    for(int k = 0; k < N/2; k++){
+        
+        complex<double> wn = (complex<double>(cos(-2*M_PI *k /N),sin(-2*M_PI *k /N)))*odd[k];
+        fftArray[k] = even[k] + wn;
+        fftArray[k+N/2] = even[k] - wn;  
+    }
+
+}
+
+// samples is of type vector of complex the same as the output
+vector<complex<double>> DFT(vector<complex<double>> data)
+{
+    int N = data.size();
+
+    // we will restore each value of X[k] into newData to then append it into the vector fftOutput
+    complex<double> newData;
+
+    vector<complex<double>> fftOutput;
+
+    // make the size of fftOutput the same as K (input size)
+    fftOutput.reserve(N);
+    
+    for(int k=0; k<N; k++){
+
+        newData = complex<double>(0,0);
+        for(int n=0; n<N; n++){
+
+            double realPart = cos(((2*M_PI)/N) * k * n);
+            double imaginaryPart = sin(((2*M_PI)/N) * k * n);
+
+            complex<double> w (realPart, -imaginaryPart);
+
+            newData += data[n] * w;
+        }
+
+        fftOutput.push_back(newData);
+    }
+
+    return fftOutput;
+
+}
---
+++ FFT/FT.py
@@ -0,0 +1,19 @@
+import ctypes
+
+
+library = ctypes.CDLL('./lib.so')
+
+library.connect()
+
+path = ['data/data1.txt', 'data/data2.txt', 'data/data3.txt',
+         'data/data4.txt', 'data/data5.txt','data/data6.txt']
+
+
+""" 
+for i in range (len(path)):
+    data = #read with path[i]
+    fft_array = data #bec fft is void function and takes the data by reference
+    #calling lines
+    dft = library.DFT(data)
+    library.FFT(fft_array)
+"""
